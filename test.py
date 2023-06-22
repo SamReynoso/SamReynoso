19d6cd8b4719b30f892f765d76df5e950468b6fd
Samuel Reynoso <samuel.g.reynoso@gmail.com>
2023-06-22 13:04:49 -0700

    Added docstring

--- test.py
+++ test.py
@@ -4,69 +4,74 @@ from gates import Gates
 
 class TestGates(unittest.TestCase):
     gates = Gates()
+    nc =gates.NAND_CONSTRUCTION
+
     data = (0,0), (0,1) ,(1,0), (1,1)    
-    
     class OutPut:    
         AND = 0, 0, 0, 1
-        NAND = 1, 1, 1, 0
         OR = 0, 1, 1, 1
         NOR = 1, 0, 0, 0
         XOR = 0, 1, 1, 0
         XNOR = 1, 0, 0, 1
+        NAND = 1, 1, 1, 0
     output = OutPut()
 
+    # Test Givin True
+    # Test Givin FALSE
     def test_boolean(self):
-        self.assertEqual(self.gates.TRUE(), 1)
-        self.assertAlmostEqual(self.gates.FALSE(), 0)
+        TRUE = 1
+        FALSE = 0
+        self.assertEqual(self.gates.TRUE, TRUE)
+        self.assertEqual(self.gates.FALSE, FALSE)
+        self.assertEqual(self.nc.TRUE, TRUE)
+        self.assertEqual(self.nc.FALSE, FALSE)
 
+    # Test Negation
     def test_not(self):
-        data = 0, 1
-        output = 1, 0
-        self.assertEqual(self.gates.NOT(data[0]), output[0])
-        self.assertEqual(self.gates.NOT(data[1]), output[1])
+        TRUE = 1
+        FALSE = 0
+        self.assertEqual(self.gates.NOT(TRUE), FALSE)
+        self.assertEqual(self.gates.NOT(FALSE), TRUE)
+        self.assertEqual(self.nc.NOT(TRUE), FALSE)
+        self.assertEqual(self.nc.NOT(FALSE), TRUE)
 
     def mapper(self, func, output):
+        """Test all 4 input pairs"""
         data = self.data
         self.assertEqual(func(data[0][0], data[0][1]), output[0])
         self.assertEqual(func(data[1][0], data[1][1]), output[1])
         self.assertEqual(func(data[2][0], data[2][1]), output[2])
-        self.assertEqual(func(data[3][0], data[3][1]), output[3])        
+        self.assertEqual(func(data[3][0], data[3][1]), output[3])    
 
+    # Test AND
     def test_and(self):
-        self.mapper(self.gates.AND, self.output.AND)
-
+        self.mapper(self.gates.AND, self.output.AND)    
+        self.mapper(self.nc.AND, self.output.AND)
+    
+    # Test OR
     def test_or(self):
         self.mapper(self.gates.OR, self.output.OR)
+        self.mapper(self.nc.OR, self.output.OR)
+    
+    # Test NOR
+    def test_nor(self):
+        self.mapper(self.gates.NOR, self.output.NOR)
+        self.mapper(self.gates.NOR, self.output.NOR)    
 
+    # Test XOR
     def test_xor(self):
         self.mapper(self.gates.XOR, self.output.XOR)
-    
+        self.mapper(self.nc.XOR, self.output.XOR)
+
+    # Test XNOR
+    def test_xnor(self):
+        self.mapper(self.gates.XNOR, self.output.XNOR)
+        self.mapper(self.nc.XNOR, self.output.XNOR)
+
+    # Test NAND
     def test_nand(self):
         self.mapper(self.gates.NAND, self.output.NAND)
+        self.mapper(self.nc.NAND, self.output.NAND)
 
-    def test_nand_not(self):
-        NOT = self.gates.NAND_CONSTRUCTION.NOT
-        data = 0, 1
-        output = 1, 0
-        self.assertEqual(NOT(data[0]), output[0])
-        self.assertEqual(NOT(data[1]), output[1])
-    
-    def test_nand_and(self):
-        AND = self.gates.NAND_CONSTRUCTION.AND
-        self.mapper(AND, self.output.AND)
-
-    def test_nand_or(self):
-        OR = self.gates.NAND_CONSTRUCTION.OR
-        self.mapper(OR, self.output.OR)
-
-    def test_nand_nor(self):
-        NOR = self.gates.NAND_CONSTRUCTION.NOR
-        self.mapper(NOR, self.output.NOR)
-    
-    def test_nand_xor(self):
-        XOR = self.gates.NAND_CONSTRUCTION.XOR
-        self.mapper(XOR, self.output.XOR)
-
-    def test_nand_xnor(self):
-        XNOR = self.gates.NAND_CONSTRUCTION.XNOR
-        self.mapper(XNOR, self.output.XNOR)
+if __name__ == "__main__":
+    unittest.main()
